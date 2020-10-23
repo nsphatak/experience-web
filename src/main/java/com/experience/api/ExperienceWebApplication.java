ee6721c471259bfd2cf1be2dd52d07ea714a97f3
Niranjan Phatak <niru.phatak@gmail.com>
2020-10-24 00:27:20 +0530

    changes to add Hystrix properties

--- src/main/java/com/experience/api/ExperienceWebApplication.java
+++ src/main/java/com/experience/api/ExperienceWebApplication.java
@@ -2,12 +2,14 @@ package com.experience.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@EnableCircuitBreaker
 @EnableHystrix
 @EnableHystrixDashboard
+@SpringBootApplication
 public class ExperienceWebApplication {
 
 	public static void main(String[] args) {
