ad37c48f038d4e865a3c5bcf472f9620c4861cd1
Niranjan Phatak <niru.phatak@gmail.com>
2020-10-26 13:27:07 +0530

    Removed Hystrix Specific Circuit Breaker Depedency to use Spring Cloud circuit breaker

--- src/main/java/com/experience/api/ExperienceWebApplication.java
+++ src/main/java/com/experience/api/ExperienceWebApplication.java
@@ -3,11 +3,9 @@ package com.experience.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableCircuitBreaker
-@EnableHystrix
 @EnableHystrixDashboard
 @SpringBootApplication
 public class ExperienceWebApplication {
