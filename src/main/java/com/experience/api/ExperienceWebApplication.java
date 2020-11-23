03e4b7878648c738d7609f8e62818c1a9d3e2b44
Niranjan Phatak <niru.phatak@gmail.com>
2020-11-23 23:47:36 +0530

    Circuit Breaker ASPECT

--- src/main/java/com/experience/api/ExperienceWebApplication.java
+++ src/main/java/com/experience/api/ExperienceWebApplication.java
@@ -2,11 +2,7 @@ package com.experience.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 @SpringBootApplication
 public class ExperienceWebApplication {
 
